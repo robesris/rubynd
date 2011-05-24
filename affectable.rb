53697495e9fb3185ec2c056feaa30891de48dd7e
Rob Esris <robesris@salamander.local>
2011-05-23 22:18:21 -0400

    Refactored out run_effects behavior into separate Affectable module.

---
+++ affectable.rb
@@ -0,0 +1,13 @@
+module Affectable
+  def run_effects(action = {})
+    # do we need these results?
+    #results = []
+    effects.each do |e|
+      response = e.respond_to_action
+      return false if response == false
+      #results << e.respond_to_action(action)
+    end
+    #results
+    true
+  end
+end
